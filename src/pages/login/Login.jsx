cbe5ab4e8353a6534e959cb25a6060ea716e368d
DipSwithGitHere <dipayan0320@gmail.com>
2023-05-22 21:37:51 +0530

    login & register page

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,41 @@
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../context/authContext";
+import "./login.scss";
+
+const Login = () => {
+  const { login } = useContext(AuthContext);
+
+  const handleLogin = () => {
+    login();
+  };
+
+  return (
+    <div className="login">
+      <div className="card">
+        <div className="left">
+          <h1>EuphoriuM</h1>
+          <p>
+          Experience the essence of euphoria at Euphorium - an enchanting social media 
+          platform that brings your moments to life. Join our thriving community, 
+          where creativity flourishes and connections thrive.
+          </p>
+          <span>Don't you have an account?</span>
+          <Link to="/register">
+            <button>Register</button>
+          </Link>
+        </div>
+        <div className="right">
+          <h1>Login</h1>
+          <form>
+            <input type="text" placeholder="Username" />
+            <input type="password" placeholder="Password" />
+            <button onClick={handleLogin}>Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
